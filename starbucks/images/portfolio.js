1df72bb939eff811bf80b89e097133a00605955c
Jadwin jG <153439193+Jadwinjg@users.noreply.github.com>
2024-06-21 23:56:59 +0530

    Add files via upload

---
+++ starbucks/images/portfolio.js
@@ -0,0 +1,19 @@
+var githubjg = new githubjg(".text", {
+  Strings: ["Frontend Developer", "Web Developer", "Editor"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
+
+{
+  /* <div class="bar">
+  <i style="color: rgb(38, 0, 255);" class="bx bxl-css3"></i>
+  <div class="info">
+    <span>CSS</span>
+  </div>
+  <div class="progress-line-css">
+    <span></span>
+  </div>
+</div>; */
+}
